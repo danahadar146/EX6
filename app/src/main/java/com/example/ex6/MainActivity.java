cd13bdf3fd4501f100b9fe6ad3ee963991fbf72b
danahadar146 <dh2184@bs.amalnet.k12.il>
2026-01-06 14:29:45 +0200

    first commit

---
+++ app/src/main/java/com/example/ex6/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.ex6;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.core.view.ViewCompat;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    private TextView txt;
+    boolean flag = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn = findViewById(R.id.btn);
+        txt = findViewById(R.id.txt);
+        txt.setText("");
+        txt.setTextSize(50);
+    }
+
+
+    public void clicked(View view) {
+        btn.setText("Click me !");
+        txt.setText("Oh, yea, I’ve been clicked!");
+    }
+}
+
